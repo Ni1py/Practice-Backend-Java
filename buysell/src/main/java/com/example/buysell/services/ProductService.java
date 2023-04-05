c7b56d0e71298d0d51ac226c624e7cc26eb7e693
Ni1py <gekepyrkov@gmail.com>
2023-04-05 21:22:47 +0300

    Начал делать учебный проект BuySell

---
+++ buysell/src/main/java/com/example/buysell/services/ProductService.java
@@ -0,0 +1,12 @@
+package com.example.buysell.services;
+
+import com.example.buysell.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private List<Product> products = new ArrayList<>();
+}
